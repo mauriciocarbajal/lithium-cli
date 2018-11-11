1487d2024ea3dc1553639da71155eea5cf69c3b5
mcarbajal@fing.edu.uy <Mauricio Carbajal>
2018-11-11 02:33:18 -0300

    constants and main idea

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 const readline = require('readline');
-const midi = require('midi');
+const easymidi = require('easymidi');
 
-var output = new midi.output();
-output.getPortCount();
-output.getPortName(0);
-output.openPort(0);
+const playChord = (output) => {
+  // Send a MIDI message.
+  output.send('noteon', {
+    note: 64,
+    velocity: 127,
+    channel: 3
+  });
+  output.send('noteon', {
+    note: 68,
+    velocity: 127,
+    channel: 3
+  });
+  output.send('noteon', {
+    note: 71,
+    velocity: 127,
+    channel: 3
+  });
+}
+
+const defineChord = (keyName, keyControl, keyShift) => {
+  console.log({
+    keyName,
+    keyControl,
+    keyShift,
+  })
+}
+
+var output = new easymidi.Output('keys-harmony-output', true);
 
 readline.emitKeypressEvents(process.stdin);
 
@@ -12,16 +36,10 @@ process.stdin.setRawMode(true);
 
 process.stdin.on('keypress', (str, key) => {
   if (key.ctrl && key.name === 'c') {
+    output.close();
     process.exit();
-    // Close the port when done.
-    output.closePort();
   } else {
-    console.log(`You pressed the "${str}" key`);
-    console.log();
-    console.log(key);
-    console.log();
-    // Send a MIDI message.
-    output.sendMessage([176,22,1]);
+    defineChord(key.name, key.ctrl, key.shift)
   }
 });
 console.log('Press any key...');
