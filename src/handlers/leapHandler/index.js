b3b0b60f073503a05cde8937c5e9385950d4292b
Mauricio <mcarbajal@fing.edu.uy>
2019-10-26 02:54:24 -0300

    basic leap handling

--- src/handlers/leapHandler/index.js
+++ src/handlers/leapHandler/index.js
@@ -5,3 +5,31 @@ const Leap = require('leapjs');
  * and includes the instrument, which is played
  * when certain conditions are met
 */
+
+const processHand = (hand) => {
+    if (hand) {
+        const {
+            palmPosition,
+            palmNormal,
+            type: handType,
+        } = hand
+
+        console.log({
+          handType,
+          palmPosition,
+          palmNormal,
+        });
+    }
+}
+
+Leap.loop((leapFrame) => {
+  const { hands } = leapFrame
+
+  if (hands.length > 0) {
+      let leftHand, rightHand
+      leftHand = hands.filter(item => (item.type === 'left'))[0]
+      rightHand = hands.filter(item => (item.type === 'right'))[0]
+      if (leftHand) processHand(leftHand)
+      if (rightHand) processHand(rightHand)
+  }
+});
